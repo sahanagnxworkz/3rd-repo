b96259f4f2824eae17b8c5b710fd3a44f903afa2
SAHANAGN <sahanagnxworkz@gmail.com>
2022-09-20 09:22:48 +0530

    update

--- SpicejetRunner.java
+++ SpicejetRunner.java
@@ -1,7 +1,7 @@
 class SpicejetRunner{
 	public static void main(String[] args){
 		System.out.println("entered main");
-		long[] flightNos={8947656,9327465490l,987987l,093876567l,87356784l,8375664l,865399l,982735243l,77789823l,8745629l};
+		long[] flightNos={8947656l,9327465490l,987987l,9387656l,87356784l,8375664l,865399l,982735243l,77789823l,8745629l};
 		Spicejet.flightNos(flightNos);
 		System.out.println(flightNos.length);
 		for(int index=0;index<flightNos.length;index++){
